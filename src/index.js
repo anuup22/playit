7ef80662b541b6aa835672f84d634c1aee9bea39
Anup <anuup22@outlook.com>
2024-03-16 10:56:10 +0530

    DB connection done

---
+++ src/index.js
@@ -0,0 +1,50 @@
+//require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*         ***** First Method to connect to the database *****
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express";
+const app = express();
+
+*** ifi: Immediately Invoked Function Expression ***
+
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error connecting to the database");
+            throw error;
+        });
+        app.listen(process.env.PORT, () => { 
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    }
+    catch (error) {
+        console.error("ERROR: ", error);
+        throw error;
+    }
+})()
+*/
